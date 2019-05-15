6f4dba3de972d73d9523669fdb42f53c908c35f5
thomasshajan7 <50335887+thomasshajan7@users.noreply.github.com>
2019-05-14 23:26:00 -0400

    Add files via upload

---
+++ introduction-to-php.php
@@ -0,0 +1,71 @@
+<?php 
+/*
++---+
+| 1 | 
++---+
+Print the sentence: Welcome to PHP!
+*/
+$hello = "Welcome To PHP!";
+echo "$hello";
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+/* 
++---+
+| 2 | 
++---+
+Print the sentence: PHP stands for "Hypertext Preprocessor"!
+*/
+$task = 'PHP stands for "Hypertext Preprocessor"!';
+echo "$task";
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+/* 
++---+
+| 3 | 
++---+
+Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
+*/
+echo"<p>PHP stands for \"Hypertext Preprocessor\"!</p>";
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+/* 
++---+
+| 4 | 
++---+
+Print the following acronyms - every acronym in a new line:
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+ 
+echo "HTML - Stands for Hypertext Markup Language<br>";
+echo "CSS - Stands for Cascading Stylesheet<br>";
+echo "JS - Stands for JavaScript<br>";
+echo "PHP - Stands for Hypertext Preprocessor<br>";
+echo "SQL - Stands for Structural Query Language";
+   
+   
+   
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+/* 
++---+
+| 5 | 
++---+
+Print the following acronyms in unordered list - <ul> ... </ul>:
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+echo "<ul>
+<li>HTML - Stands for Hypertext Markup Language</li>
+<li>CSS - Stands for Cascading Stylesheet</li>
+<li>JS - Stands for JavaScript</li>
+<li>PHP - Stands for Hypertext Preprocessor</li>
+<li>SQL - Stands for Structural Query Language</li>
+</ul>";
+?>
